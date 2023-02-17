5d522e845d47e61dd07feb359d66742cc52b2d76
root <loice.lovie@gmail.com>
2023-02-17 09:30:34 -0800

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,15 +18,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("is positive");
+		printf(" is positive");
 	}
 	else if (n == 0)
 	{
-		printf("is negative");
+		printf(" is negative");
 	}
 	else if (n < 0)
 	{
-		printf("is zero");
+		printf(" is zero");
 	}
 	return (0);
 }
