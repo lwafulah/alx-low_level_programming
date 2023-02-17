7d8586db42b8cb7ec871cc99f230edb55762e5c8
root <loice.lovie@gmail.com>
2023-02-16 19:54:07 -0800

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
- * Return Always 0 (Success)
-*/
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-	
-	char str[] = ("\"Programming is like building a multilingual puzzle\n");
-	puts(str);
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle"); 
+	return (0);
 }
