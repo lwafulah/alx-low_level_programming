fee20cd6577929cab493f5acc031eae929e3707f
root <loice.lovie@gmail.com>
2023-02-17 11:20:49 -0800

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,12 +3,12 @@
 #include <time.h>
 
 /**
- * main - assign a random number variable n 
+ * main - assign a random number variable n
  *
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 	int n;
 
@@ -17,15 +17,15 @@ int main (void)
 
 	if (n > 5)
 	{
-		printf("Last digit of n is %d and is greater than 5\n", n);
+		printf("%d Last digit of n is n and is greater than 5\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("Last digit of n is %d is and is 0\n", n);
+		printf("%d Last digit of n is n is and is 0\n", n);
 	}
 	else if (n < 6 && !n == 0)
 	{
-		printf("Last digit of n is %d and is less than 6 and not 0\n", n);
+		printf("%d Last digit of n is n and is less than 6 and not 0\n", n);
 	}
 	return (0);
 }
