ee33b817e2d2c4cd5b3a3d40a152ed11850afc15
root <loice.lovie@gmail.com>
2023-02-18 01:25:27 -0800

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - prints all single digit numbers of base 10 starting from 0 using putchar
+ * main - prints all single digit numbers of base 10 from 0 using putchar
  *
  * Return: Always 0 (Success)
  */
