d37c876904bcee9278b48129204a3d5d6532b37f
root <loice.lovie@gmail.com>
2023-02-20 10:24:55 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
@@ -7,6 +9,6 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-    return (1);
+	write(2, "\"And that piece of art is useful\"- Dora Korpar,2015-10-19\n", 56);
+	return (1);
 }
