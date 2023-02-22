dd74136a496c3fd6e21bbad46b3fe52ac89c1f6f
root <loice.lovie@gmail.com>
2023-02-21 20:57:29 -0800

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - checks the absolute value of an integer
+ * @x : paramter to be tested
+ * Return: Always x
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+	{
+		return (x);
+	}
+	else
+	{
+		return (-x);
+	}
+}
