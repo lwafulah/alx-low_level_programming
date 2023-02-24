fb30d72b307d27121c53ea26022849296e011bc7
root <loice.lovie@gmail.com>
2023-02-24 10:52:03 -0800

    nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * Return: The number 0 to 9
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 0; c <= 0; c++)
+	{
+		_putchar(c + '0');
+	}
+	_putchar('\n');
+}
