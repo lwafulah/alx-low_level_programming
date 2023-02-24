a5231aeeca5b95638b987e4fa25e2c45377c658a
root <loice.lovie@gmail.com>
2023-02-24 12:27:16 -0800

    nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,28 +6,28 @@
  */
 void print_diagonal(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		int i, j;
+	int i, j;
 
+	if (n > 0)
+	{
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j <= i; j++)
 			{
 				if (j == i)
 				{
 					_putchar('\\');
 				}
-				else if (j < i)
+				else
 				{
-					_putchar(' ');
+					 _putchar(' ');
 				}
-				_putchar('\n');
 			}
+			_putchar('\n');
 		}
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }
