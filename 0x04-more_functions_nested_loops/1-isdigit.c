948531b4defb1f19159be986d0118d21dd93e50f
root <loice.lovie@gmail.com>
2023-02-24 03:57:38 -0800

    task 2

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _isdigit - function that checks for a digit (0 through 9).
  * @c: parameter to be tested
@@ -8,11 +9,7 @@ int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 	{
-		return(1);
+	return (1);
 	}
-	else
-	{
-		return (0);
-	}
-
+	return (0);
 }
