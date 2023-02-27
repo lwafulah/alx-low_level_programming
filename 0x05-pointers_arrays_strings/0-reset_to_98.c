c1c5907c73aff539c48167fcf4728b38cbadbc5e
root <loice.lovie@gmail.com>
2023-02-27 09:56:07 -0800

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to an int & updates the value it points to 98
+ * @n: number to be updated
+ * Return: 0 on success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
