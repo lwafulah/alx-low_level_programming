cd29f62b35b776eab1167c0a2de9eba914807171
root <loice.lovie@gmail.com>
2023-02-27 12:04:43 -0800

    every other character of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,16 +2,17 @@
 
 /**
  * puts2 - prints every other character of a string
- * @str: string to b used
+ * @str: string to be used
  * Return: 0 on success
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
+		i += 2;
 	}
 	_putchar('\n');
 }
