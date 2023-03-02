84007c1cb18a9eec5a90ddd69723a33534aa814f
root <loice.lovie@gmail.com>
2023-03-02 11:40:24 -0800

    leet

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,9 +6,7 @@
  */
 char *cap_string(char *str)
 {
-	int i = 0;
-
-	while (str[i] != '\0')
+	for (int i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ','
 				|| str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?'
@@ -19,7 +17,6 @@ char *cap_string(char *str)
 			if (str[i] >= 'a' && str[i] <= 'z')
 				str[i] = str[i] - 32;
 		}
-		i++;
 	}
 	return (str);
 }
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string to 1337.
+ * @s: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *leet(char *p)
+{
+	int i, j;
+	char letters[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char numbers[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	for (i = 0; p[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (p[i] == letters[j])
+				p[i] = numbers[j];
+		}
+	}
+	return (p);
+}
