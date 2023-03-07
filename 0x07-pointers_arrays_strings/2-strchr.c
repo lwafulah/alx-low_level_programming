13742bf7e4576d07aa6406815336678770628729
lwafulah <loice.lovie@gmail.com>
2023-03-07 07:10:08 -0800

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,16 +3,20 @@
  * *_strchr -  function that locates a character in a string
  * @s: string to be tested
  * @c: character to be found
- * Return: pointer to the first occurrence of c in the string s or NULL
+ * Return:Return: pointer to the first occurrence of the character c
+ * in the string s,or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s != '\0')
 	{
-		if (s[i] == c)
-		return (s + i);
+		if (*s == c)
+			return (s);
+		s++;
 	}
+
+	if (*s == c)
+		return (s);
+
 	return (NULL);
 }
