addda63d8b49bd84a3e325fdede27ee90ca63de4
lwafulah <loice.lovie@gmail.com>
2023-03-22 10:38:29 -0700

    What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * print_name - function that prints a name
  * @f: pointer to the function that takes a char pointer as an arguement
