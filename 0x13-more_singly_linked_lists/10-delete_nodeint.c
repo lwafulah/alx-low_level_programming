8b1f05dea066484432b7cc98fea27dae4172fa5d
root <loice.lovie@gmail.com>
2023-04-05 15:00:00 -0700

    updated

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,7 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		*head = (*head)->next;
 		temp->next = NULL;
 		free(temp);
-		return (-1);
+		return (1);
 	}
 	else
 	{
@@ -31,6 +31,10 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			if (temp != NULL)
 			{
 				temp = temp->next;
+			}
+			else
+			{
+				return (-1);
 			}
 		}
 	if (temp != NULL && temp->next != NULL)
