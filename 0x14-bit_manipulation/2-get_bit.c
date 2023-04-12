93f19bdbb49af9da0efc5865b21a6f23f517f06b
root <loice.lovie@gmail.com>
2023-04-12 06:47:28 -0700

    updated

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,5 +8,9 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if( N & (1 << i) )
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	return ((n >> index) & 1);
 }
