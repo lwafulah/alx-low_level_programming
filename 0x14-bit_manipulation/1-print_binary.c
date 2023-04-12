9c319bdf61c2a92be8355d7bf8046f6174b0edde
root <loice.lovie@gmail.com>
2023-04-12 06:02:04 -0700

    updated

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,7 +9,26 @@ void print_binary(unsigned long int n)
 {
 	if (n == 0)
 		return (0);
-	if (n > 1)
-		print_binary(n >> '1');
-	_putchar(n & '1');
+
+	int count;
+	unsigned long int mask;
+
+	mask = n;
+	count = 0;
+
+	while (mask > 0)
+	{
+		mask >>= 1;
+		count++;
+	}
+	while (count > 0)
+	{
+		count--;
+		mask = n >> count;
+
+		if (mask & 1)
+			_putchar ('1');
+		else
+			_putchar ('0');
+	}
 }
